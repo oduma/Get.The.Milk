cfae626354a7183ac5079a8b4657d229a1d1cc8b
oduma <tavi_duma2002@yahoo.co.uk>
2014-09-02 13:31:41 +0100

    Cleaned up the Factories

--- GetTheMilk/Factories/ActionsFactory.cs
+++ GetTheMilk/Factories/ActionsFactory.cs
@@ -1,10 +1,7 @@
 ﻿using GetTheMilk.Actions.ActionTemplates;
 using Sciendo.IOC;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GetTheMilk.Factories
 {
